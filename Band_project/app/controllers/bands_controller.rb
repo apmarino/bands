f1df310fcc9c364536c727257ef8c137c1b1c495
Viktoria Szontagh <vikkiszontagh@gmail.com>
2015-08-25 15:39:53 -0400

    controllers updated

--- Band_project/app/controllers/bands_controller.rb
+++ Band_project/app/controllers/bands_controller.rb
@@ -1,3 +1,41 @@
 class BandsController < ApplicationController
+
+  def index
+    @bands = Band.all
+  end
   
+  def new
+    @band = Bands.new
+  end
+
+  def create
+    Band.create(band_params)
+    redirect_to band_path
+  end
+
+  def show
+    @band = Band.find(params[:id])
+  end
+
+  def edit
+    @band = Band.find(params[:id])
+  end
+
+  def update
+    band = Band.find(params[:id])
+    band.update(band_params)
+    redirect_to band_path
+  end
+
+  def destroy
+    band = Band.find(params[:id])
+    band.destroy
+    redirect_to band_path
+  end
+
+  private
+  def band_params
+    params.require(:band).permit(:name, :genre, :explicit_lyrics, :venue_id)
+  end
+
 end
--- Band_project/app/controllers/venues_controller.rb
+++ Band_project/app/controllers/venues_controller.rb
@@ -1,2 +1,40 @@
 class VenuesController < ApplicationController
+  
+  def index
+    @venues = Venue.all
+  end
+  
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    Venue.create(venue_params)
+    redirect_to venue_path
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    venue = Venue.find(params[:id])
+    venue.update(venue_params)
+    redirect_to venue_path
+  end
+
+  def destroy
+    venue = Venue.find(params[:id])
+    venue.destroy
+    redirect_to venue_path
+  end
+
+  private
+  def venue_params
+    params.require(:venue).permit(:name, :city, :state, :family_friendly)
+  end
 end
