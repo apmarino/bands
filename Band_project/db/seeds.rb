445509c4697f3b95f222882a65c63da8cc386dee
Alexander Marino <marinoalexp@gmail.com>
2015-08-25 17:07:31 -0400

    misc/

--- Band_project/db/seeds.rb
+++ Band_project/db/seeds.rb
@@ -12,8 +12,8 @@ Event.delete_all
 
 venue = Venue.create({name:"House of Yes", city:"New York", state:"NJ", family_friendly:false})
 
-band = Band.create({name:"Objects", genre:"rock", explicit_lyrics:false, venue_id:1})
+band = Band.create({name:"Objects", genre:"rock", explicit_lyrics:false, venue_id: venue.id})
 
-event = Event.create({date:"8/27/2015", alcohol_served: true, band_id:1, venue_id:1})
+event = Event.create({date:"8/27/2015", alcohol_served: true, band_id: band.id, venue_id: venue.id})
 
 
