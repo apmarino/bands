baac2cf26c7cab632f9e26e45d2955d809b02b60
Viktoria Szontagh <vikkiszontagh@gmail.com>
2015-08-25 17:19:07 -0400

    venues show works

--- Band_project/db/seeds.rb
+++ Band_project/db/seeds.rb
@@ -10,7 +10,7 @@ Venue.delete_all
 Band.delete_all
 Event.delete_all
 
-venue = Venue.create({name:"House of Yes", city:"New York", state:"NJ", family_friendly:false})
+venue = Venue.create({name:"House of Yes", city:"New York", state:"NY", family_friendly:false})
 
 band = Band.create({name:"Objects", genre:"rock", explicit_lyrics:false, venue_id:1})
 
