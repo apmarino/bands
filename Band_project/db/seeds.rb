94fa81f7043b83894728771268135483d0eaa25e
James Leisy <jamesleisy@gmail.com>
2015-08-25 19:26:19 -0400

    seed data

--- Band_project/db/seeds.rb
+++ Band_project/db/seeds.rb
@@ -6,14 +6,25 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Venue.delete_all
-Band.delete_all
 Event.delete_all
+Band.delete_all
+Venue.delete_all
 
-venue = Venue.create({name:"House of Yes", city:"New York", state:"NY", family_friendly:false})
+venue1 = Venue.create({name:"House of Yes", city:"New York", state:"NY", family_friendly:false})
+venue2 = Venue.create({name:"Barclay Center", city:"New York", state:"NY", family_friendly:false})
+venue3 = Venue.create({name:"Jake's Backyard BBQ", city:"New York", state:"NY", family_friendly:false})
 
-band = Band.create({name:"Objects", genre:"rock", explicit_lyrics:false, venue_id: venue.id})
+band1 = Band.create({name:"Objects", genre:"rock", explicit_lyrics:false, venue_id: venue1.id})
+band2 = Band.create({name:"Pink Floyd", genre:"rock", explicit_lyrics:true, venue_id: venue2.id})
+band3 = Band.create({name:"The Foundations", genre:"soul", explicit_lyrics:false, venue_id: venue1.id})
+band4 = Band.create({name:"The Zombies", genre:"rock", explicit_lyrics:false, venue_id: venue2.id})
+band5 = Band.create({name:"Bon Jovi", genre:"rock", explicit_lyrics:false, venue_id: venue3.id})
 
-event = Event.create({date:"2015-8-27", alcohol_served: true, band_id: band.id, venue_id: venue.id})
+
+event1 = Event.create({date:"2015-8-27", alcohol_served: true, band_id: band1.id, venue_id: venue1.id})
+event2 = Event.create({date:"2015-9-10", alcohol_served: true, band_id: band2.id, venue_id: venue2.id})
+event3 = Event.create({date:"2015-9-14", alcohol_served: true, band_id: band3.id, venue_id: venue1.id})
+event4 = Event.create({date:"2015-10-21", alcohol_served: true, band_id: band4.id, venue_id: venue2.id})
+event4 = Event.create({date:"2015-10-21", alcohol_served: true, band_id: band5.id, venue_id: venue3.id})
 
 
